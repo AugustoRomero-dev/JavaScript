6e28bb528b12339f898ec6ecaced7ad275a0a45e
AugustoRomero-dev <gutocordeiroromero@gmail.com>
2026-03-08 14:44:17 -0400

    atualizado

---
+++ Aulas JS/aula 14(EX)/javascript.js
@@ -0,0 +1,14 @@
+function verificar(){
+    var ini = document.getElementById(ini)
+    var fim = document.getElementById(ifim)
+    var pass = document.getElementById(ipasso)
+    var res = document.getElementById(res)
+    if(ini == 0 && fim == 0 & pass == 0){
+        alert('Digite um numero')
+    }
+
+}
+//recer inicio
+//recerber fim
+//receber passo
+//
