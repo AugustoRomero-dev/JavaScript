b84f6dc15e924af1893a64d142511c2f93032552
AugustoRomero-dev <gutocordeiroromero@gmail.com>
2026-03-24 23:04:39 -0400

    atualizado

--- exchat/array 05/ambiente.js
+++ exchat/array 05/ambiente.js
@@ -1,9 +1,41 @@
 let numeros = [10, 5, 20, 8, 15]
 
-let maior = [0]
+// maior numero
+let primeiroN = numeros[0]
 for (let num of numeros){
-    if(num > maior){
+    if(num > primeiroN){
+        primeiroN = num
         
-        console.log(maior)
     }
 }
+console.log(`maior nuermo: ${primeiroN}`)
+
+console.log('...')
+
+
+
+// menor n
+let menorN = numeros[0]
+for (let num of numeros){
+    if(num < menorN){
+        menorN = num
+       
+    }
+}
+console.log(`menor numero: ${menorN}`)
+
+console.log('...')
+
+
+
+
+
+// soma
+let soma = 0
+for (let num of numeros){
+   
+    soma += num
+    
+}
+console.log(`a soma total e: ${soma}`)
+
