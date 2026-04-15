c5d64a397e2ce7b0fc87513ef620c2cecf931c56
AugustoRomero-dev <gutocordeiroromero@gmail.com>
2026-04-14 22:49:37 -0400

    atualizado

--- exchat/desafio.treino 2/Javascript.js
+++ exchat/desafio.treino 2/Javascript.js
@@ -1,8 +1,5 @@
 let lista = [] //VARIAVEL GLOBAL
 
-let numero = document.getElementById('inum') //adicionar
-let n = Number(numero.value) //adicionar
-
 function Adicionar(){
     let numero = document.getElementById('inum') //adicionar
     let n = Number(numero.value) //adicionar
@@ -30,14 +27,58 @@ function mostrar(){
 
     }
 }
+
+
 function Finalizar(){
+    let resultado = document.getElementById('res')
+
+    if (lista.length === 0){
+        alert('Adicione números antes!')
+        return
+    }
+
+    resultado.innerHTML = ''
+
+    // quantidade
+    let total = lista.length
+
+    // maior
+    let maior = lista[0]
+
+    // menor
+    let menor = lista[0]
+
+    // soma
+    let soma = 0
+
+    for (let num of lista){
+        soma += num
+
+        if (num > maior){
+            maior = num
+        }
+
+        if (num < menor){
+            menor = num
+        }
+    }
+
+    let media = soma / total
+
+    resultado.innerHTML += `Total: ${total} <br>`
+    resultado.innerHTML += `Maior: ${maior} <br>`
+    resultado.innerHTML += `Menor: ${menor} <br>`
+    resultado.innerHTML += `Soma: ${soma} <br>`
+    resultado.innerHTML += `Média: ${media} <br>`
+}
+/*function Finalizar(){
     let numero = document.getElementById('inum') //ADICIONAR
     let n = Number(numero.value) //ADICIONAR
     let resultado = document.getElementById('res') // MOSTRAR
 
     for(let Nlista in lista){ // QUANTIDADE DE NUMEROS
        
-        resultado.innerHTML = `foram digitados ${Ntotal} numeros`
+        resultado.innerHTML = `foram digitados ${Nlista} numeros`
     }
 
 
@@ -61,8 +102,20 @@ function Finalizar(){
         }
     }
 
+    for(let num of lista){ // soma total
+        let soma = lista[0] += num
+        resultado.innerHTML = `A soma e ${soma}`
+    }
+
+     for(let num of lista){ // MEDIA
+        let soma = lista[0] += num
+        let media = soma % 2
+        resultado.innerHTML = `A soma e ${media}`
+    }
+
+
 
 
 
    
-}
+}*/
