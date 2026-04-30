5f91e7f99e6de30922228daeebcc29a66e382e5a
AugustoRomero-dev <gutocordeiroromero@gmail.com>
2026-04-29 22:18:02 -0400

    29/04

--- exchat/lista de tarefa/Javascript.js
+++ exchat/lista de tarefa/Javascript.js
@@ -3,14 +3,24 @@ let lista = []
 function Adicionar(){
     let tarefa = document.getElementById("tarefa")
     let listaHTML = document.getElementById('lista')
+    let cont = document.getElementById('contador')
 
-    if(tarefa.value.length === 0 ){
+    if(tarefa.value.length === 0 ){ // input vazio nao aceita
         alert('Adicione uma tarefa')
         return
     }
+    //if(lista.includes(tarefa)){ // verifica item igual
+        //alert('ja possui esse item na sua lista')
+       // return
+    //}
     lista.push(tarefa)
+    tarefa.value = ''//limpa input
 
-    let item = document.createElement('li')
-    item.text = `Tarefa ${tarefa} adicionada`
-    listaHTML.appendChild(li)
+    let item = document.createElement('li') // cria um <li> (item de lista) na memória
+
+    item.textContent = `Tarefa ${tarefa} adicionada`//coloca texto dentro do <li>
+
+    listaHTML.appendChild(item)
+    //listaHTML.innerHTML = `possui ${lista.length} tarefas`
+    item.innerText = `${tarefa}`
 }
