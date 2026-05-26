1b1b2384e2208fd4694e45ddb99fd5fbbd48e60b
AugustoRomero-dev <gutocordeiroromero@gmail.com>
2026-05-25 22:41:01 -0300

    notebook

---
+++ Aula Chat/map.js
@@ -0,0 +1,15 @@
+//let numeros = [5, 10, 15]
+
+//const array = numeros.map(function(numero){
+  //  return numero * 2
+//})
+//console.log(array)
+
+
+
+const nomes = ["Augusto", 'Pedro', 'Mj']
+
+const maiusculo = nomes.map(function(nome){
+   return nome.toUpperCase()
+})
+console.log(maiusculo)
