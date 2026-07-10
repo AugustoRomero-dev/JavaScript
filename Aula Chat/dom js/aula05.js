014452149dffd3fdb1a491a26fd2cc853d71ecd4
AugustoRomero-dev <gutocordeiroromero@gmail.com>
2026-07-10 00:12:02 -0400

    pc

---
+++ Aula Chat/dom js/aula05.js
@@ -0,0 +1,40 @@
+const tarefas = document.getElementById('tarefa')
+const botao = document.getElementById("botao")
+const listaUL = document.getElementById("lista")
+const res = document.getElementById('res')
+
+let arrayTarefa = []
+
+botao.addEventListener('click', ()=>{
+    if(tarefas.value.trim() === ''){
+        alert('Digite uma tarefa')
+        return
+    }
+    if(arrayTarefa.includes(tarefas.value)){
+        alert('Essa tarefa ja existe')
+        return
+    }
+
+    arrayTarefa.push(tarefas.value)
+    tarefas.value = ''
+
+    renderizar()
+})
+
+function renderizar(){
+    listaUL.innerHTML = ''
+    arrayTarefa.forEach((item, posicao) =>{
+        const li = document.createElement('li')
+        li.textContent = item
+        
+
+        li.addEventListener('click', ()=>{
+            arrayTarefa.splice(posicao, 1)
+            renderizar()
+        })
+        listaUL.appendChild(li)
+        res.innerHTML = `Vc pososui ${arrayTarefa.length} tarefa`
+    })
+   
+
+}
