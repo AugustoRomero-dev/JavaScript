b2456ac1a29d6317f9687c0a9c85d239fcb99cf4
AugustoRomero-dev <gutocordeiroromero@gmail.com>
2026-07-20 22:46:32 -0400

    ex dom e log

---
+++ exchat/luzes.js
@@ -0,0 +1,13 @@
+ let estadoLuzes = [
+      { comodo: "Sala", ligada: false },
+      { comodo: "Cozinha", ligada: false },
+      { comodo: "Quarto", ligada: false }
+    ]
+
+    const listaLuzes = document.getElementById('luzes')
+
+    // seu código vai aqui embaixo
+    listaLuzes.addEventListener('click', (event)=>{
+        const posicaoClicada = event.target.data.id 
+        
+    })
