f51c87b0108fccbe7b791f97369917661e02b151
AugustoRomero-dev <gutocordeiroromero@gmail.com>
2026-08-03 22:43:19 -0400

    .

--- fetch()/aula04ex.js
+++ fetch()/aula04ex.js
@@ -1,12 +1,15 @@
 async function buscarTarefasUsuario() {
     try{
-        const resposta = await fetch('https://jsonplaceholder.typicode.com/users/1/todos')
+        const resposta = await fetch('https://jsonplaceholder.typicode.com/users/99999/todos')
         if(!resposta.ok){
             throw new Error("Nao foi Possivel Carregar As Tarefas!")
 
             
         }
         const dado = await resposta.json()
+        if (dado.length === 0) {
+             throw new Error('Nenhuma tarefa encontrada para esse usuário')
+        }
         dado.forEach((item, posicao) => {
             if(item.completed === false){
                 console.log(item,  '-PENDENTE')
