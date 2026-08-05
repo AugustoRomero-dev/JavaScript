548567cdf8ac0dc419c7326d7ce0299e386e28da
AugustoRomero-dev <gutocordeiroromero@gmail.com>
2026-08-04 22:44:20 -0400

    filter

---
+++ fetch()/aula05ex.js
@@ -0,0 +1,19 @@
+async function buscarAlbuns() {
+    try{ 
+        const resposta = await fetch('https://jsonplaceholder.typicode.com/users/2/albums')
+        if(!resposta.ok){
+            throw new Error("Nao localizei o album");
+            
+        }
+        const dado = await resposta.json()
+        if(dado.length === 0){
+             throw new Error('Nenhuma tarefa encontrada para esse usuário')
+        }
+        const dadoNovo = dado.filter(item => item.title.length > 20)
+        console.log(dadoNovo)
+
+    } catch(erro){
+        console.log('Deu Erro:', erro.message)
+    }
+}
+buscarAlbuns()
