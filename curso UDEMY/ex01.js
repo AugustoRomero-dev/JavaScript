048da5b647551a54c565e6986a2e2f67e2af4c39
AugustoRomero-dev <gutocordeiroromero@gmail.com>
2026-08-20 22:36:42 -0400

    udemy pt1

---
+++ curso UDEMY/ex01.js
@@ -0,0 +1,27 @@
+function escopo() {
+
+
+    const form = document.getElementById('form')
+    const resultado = document.querySelector('.resultado')
+
+    const pessoas = []
+
+    function recebeEvento(evento){
+        evento.preventDefault()
+
+        const nome = document.getElementById('nome')
+        const Sobrenome = document.getElementById('Sobrenome')
+        const nome = document.getElementById()
+        const nome = document.getElementById()
+
+    }
+
+    form.addEventListener('submit', recebeEvento)
+
+
+
+
+
+
+
+}
